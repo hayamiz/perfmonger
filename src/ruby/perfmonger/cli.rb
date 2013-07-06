14fc45369758f3ef6ac128dca9dcc1a8e06f5524
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-07-07 00:33:04 +0900

    [perfmonger.rb] remove comments

--- src/ruby/perfmonger/cli.rb
+++ src/ruby/perfmonger/cli.rb
@@ -57,57 +57,5 @@ EOS
   end
 end
 
-# class StatCommand < RecordCommand
-#   register_command 'stat'
-# 
-#   def initialize
-#     super
-#     @logfile = "./perfmonger.log"
-#   end
-# 
-#   def run(argv)
-#     setup_parser()
-#     @parser.parse!(argv)
-#     @command = argv
-# 
-#     if ! @report_io && ! @report_ctx_switch
-#       @report_cpu = true
-#     end
-# 
-#     exec_stat_cmd()
-#   end
-# 
-# private
-#   def setup_parser()
-#     super
-#     @parser.banner = <<EOS
-# Usage: #{File.basename($0)} stat [options] -- COMMANDS
-# 
-# Options:
-# EOS
-#   end
-# 
-#   def exec_stat_cmd()
-#     cmd = File.expand_path("../perfmonger-record", __FILE__)
-# 
-#     args = []
-#     args << '-i'
-#     args << @interval.to_s
-#     args << '-C' if @report_cpu
-#     args << '-S' if @report_ctx_switch
-#     args << '-l' if @logfile != STDOUT
-#     args << @logfile if @logfile != STDOUT
-#     @devices.each do |device|
-#       args << '-d'
-#       args << device
-#     end
-#     args << 'v' if @verbose
-# 
-#     record_pid = Process.spawn(cmd, *args)
-#     system(*@command)
-#     Process.kill("INT", record_pid)
-#   end
-# end
-
 end
 end
