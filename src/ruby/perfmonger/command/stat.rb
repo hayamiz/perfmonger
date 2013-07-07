085a107047720e08956b3ee78647393e79bc80c9
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-07-08 01:10:04 +0900

    [stat] show CPU usage summary

--- src/ruby/perfmonger/command/stat.rb
+++ src/ruby/perfmonger/command/stat.rb
@@ -57,32 +57,64 @@ class StatCommand < BaseCommand
         nil
       end
     end.compact
-    records.shift # removed first all-zero line
+    header_record = records.shift # removed first all-zero line
 
     puts("== Performance summary of '#{@argv.join(" ")}' ==")
     printf("Execution time: %.4f\n", @end_time - @start_time)
 
-    if records.first['ioinfo']
+    if header_record && header_record['cpuinfo']
+      p records
+      p records.map
+      usr, sys, iowait, irq, soft, other = records.map do |record|
+        [record['cpuinfo']['all']['%usr'] + record['cpuinfo']['all']['%nice'],
+         record['cpuinfo']['all']['%sys'],
+         record['cpuinfo']['all']['%iowait'],
+         record['cpuinfo']['all']['%irq'],
+         record['cpuinfo']['all']['%soft'],
+         [100.0 - record['cpuinfo']['all']['%idle'], 0.0].max]
+      end.inject do |a, b|
+        (0..5).map do |idx|
+          a[idx] + b[idx]
+        end
+      end.map do |sum|
+        sum / records.size.to_f
+      end.map do |avg|
+        sprintf("%.4f", avg)
+      end
+
+      puts("")
+      puts("* CPU USAGE")
+      puts("     %usr: #{usr || 'N/A'}")
+      puts("     %sys: #{sys || 'N/A'}")
+      puts("  %iowait: #{iowait || 'N/A'}")
+      puts("     %irq: #{irq || 'N/A'}")
+      puts("    %soft: #{soft || 'N/A'}")
+      puts("   %other: #{other || 'N/A'}")
+    end
+
+    if header_record && header_record['ioinfo']
       records.first['ioinfo']['devices'].each do |device|
         r_iops, w_iops, r_sec, w_sec = records.map do |record|
           [record['ioinfo'][device]['r/s'],
            record['ioinfo'][device]['w/s'],
-           record['ioinfo'][device]['rsec/s'],
-           record['ioinfo'][device]['wsec/s']]
+           record['ioinfo'][device]['rsec/s'] * 512 / 1024.0 / 1024.0,
+           record['ioinfo'][device]['wsec/s'] * 512 / 1024.0 / 1024.0]
         end.inject do |a, b|
           (0..3).map do |idx|
             a[idx] + b[idx]
           end
         end.map do |sum|
           sum / records.size.to_f
+        end.map do |avg|
+          sprintf("%.4f", avg)
         end
 
         puts("")
         puts("* DEVICE: #{device}")
-        printf("        avg. read IOPS: %.4f\n", r_iops)
-        printf("       avg. write IOPS: %.4f\n", w_iops)
-        printf("  avg. read throuhgput: %.4f MB/s\n", r_sec * 512 / 1024.0 / 1024.0)
-        printf(" avg. write throughput: %.4f MB/s\n", w_sec * 512 / 1024.0 / 1024.0)
+        puts("        avg. read IOPS: #{r_iops || 'N/A'}")
+        puts("       avg. write IOPS: #{w_iops || 'N/A'}")
+        puts("  avg. read throughput: #{r_sec || 'N/A'} MB/s")
+        puts(" avg. write throughput: #{w_sec || 'N/A'} MB/s")
       end
     end
   end
