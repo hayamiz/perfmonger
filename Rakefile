49c161f609b09cc6ad56ef33ef17a1b14cdc5577
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-07-15 17:50:19 +0900

    use RSpec for testing ruby library part

---
+++ Rakefile
@@ -0,0 +1,8 @@
+
+require 'rubygems'
+require 'rspec/core/rake_task'
+
+task :default => [:spec]
+
+desc "Run all specs in spec directory"
+RSpec::Core::RakeTask.new(:spec)
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+$LOAD_PATH << File.expand_path('../../src/ruby', __FILE__)
+
+require 'perfmonger'
+
+RSpec.configure do |config|
+  # RSpec config here
+end
