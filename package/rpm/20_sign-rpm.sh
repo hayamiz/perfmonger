ab16195586f90934d8fa05830a97d1568374c3a2
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-12-03 22:47:46 +0900

    add old rpms as archives

--- package/rpm/20_sign-rpm.sh
+++ package/rpm/20_sign-rpm.sh
@@ -4,7 +4,7 @@ this_dir=$(dirname $(readlink -f $0))
 
 set -ev
 
-for rpmfile in $(find $this_dir -name '*.rpm' -type f); do
+for rpmfile in $(find $this_dir/centos -name '*.rpm' -type f); do
     rpm -D "_gpg_name 981A94C0" \
             --resign $rpmfile
 done
