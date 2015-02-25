e46057dce2521af1094784fa3a1c27b8d0cd74fc
Yuto Hayamizu <y.hayamizu@gmail.com>
2015-02-26 03:32:34 +0900

    add tests for GetCpuUsage

--- core/subsystem/usage_test.go
+++ core/subsystem/usage_test.go
@@ -50,3 +50,84 @@ func TestGetCoreUsage(t *testing.T) {
 		t.Errorf("usage.Sys = %v, want 25.0", usage.User)
 	}
 }
+
+func TestGetCpuUsage(t *testing.T) {
+	var err error
+	var usage *CpuUsage
+
+	num_core := 2
+	c1 := NewCpuStat(num_core)
+	c2 := NewCpuStat(num_core)
+
+	usage, err = GetCpuUsage(c1, c2)
+	if err == nil {
+		t.Error("Error should be returned because no difference between c1 and c2")
+	}
+	if usage != nil {
+		t.Error("Nil should be returned as usage")
+	}
+
+	c1.CoreStats[0].User = 100
+	c1.CoreStats[0].Sys = 50
+	c1.CoreStats[1].User = 100
+	c1.CoreStats[1].Sys = 50
+
+	c2.CoreStats[0].User = c1.CoreStats[0].User + 300
+	c2.CoreStats[0].Sys = c1.CoreStats[0].Sys + 100
+	c2.CoreStats[1].User = c1.CoreStats[0].User + 300
+	c2.CoreStats[1].Sys = c1.CoreStats[0].Sys + 100
+
+	usage, err = GetCpuUsage(c1, c2)
+	if err == nil {
+		t.Error("Error should be returned because no progress in .All uptime")
+	}
+	if usage != nil {
+		t.Error("Nil should be returned as usage")
+	}
+
+	c1.All.User = 200
+	c1.All.Sys = 100
+	c2.All.User = c1.All.User + 600
+	c2.All.Sys = c1.All.Sys + 200
+
+	c1.CoreStats[0].Clear()
+	c2.CoreStats[0].Clear()
+
+	usage, err = GetCpuUsage(c1, c2)
+	if err == nil {
+		t.Error("Error should be returned because no progress in .CoreStats[9] uptime")
+	}
+	if usage != nil {
+		t.Error("Nil should be returned as usage")
+	}
+
+	c1.CoreStats[0].User = 100
+	c1.CoreStats[0].Sys = 50
+	c2.CoreStats[0].User = c1.CoreStats[0].User + 300
+	c2.CoreStats[0].Sys = c1.CoreStats[0].Sys + 100
+	usage, err = GetCpuUsage(c1, c2)
+	if err != nil {
+		t.Error("Error should not be returned.")
+	}
+	if usage == nil {
+		t.Error("usage == nil, want non-nil")
+	}
+	if math.Abs(usage.All.User-150.0) > 0.01 {
+		t.Errorf("usage.User = %v, want 150.0", usage.All.User)
+	}
+	if math.Abs(usage.All.Sys-50.0) > 0.01 {
+		t.Errorf("usage.Sys = %v, want 50.0", usage.All.User)
+	}
+	if math.Abs(usage.CoreUsages[0].User-75.0) > 0.01 {
+		t.Errorf("usage.User = %v, want 75.0", usage.CoreUsages[0].User)
+	}
+	if math.Abs(usage.CoreUsages[0].Sys-25.0) > 0.01 {
+		t.Errorf("usage.Sys = %v, want 25.0", usage.CoreUsages[0].User)
+	}
+	if math.Abs(usage.CoreUsages[1].User-75.0) > 0.01 {
+		t.Errorf("usage.User = %v, want 75.0", usage.CoreUsages[1].User)
+	}
+	if math.Abs(usage.CoreUsages[1].Sys-25.0) > 0.01 {
+		t.Errorf("usage.Sys = %v, want 25.0", usage.CoreUsages[1].User)
+	}
+}
