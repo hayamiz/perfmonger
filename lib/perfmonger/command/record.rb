f49f8ee2cea8cf9b9aeec0a112c7a99d0a1378da
Yuto Hayamizu <y.hayamizu@gmail.com>
2016-11-23 04:16:25 +0900

    [record] Error handling in --kill processing (no session, already dead session PID)

--- lib/perfmonger/command/record.rb
+++ lib/perfmonger/command/record.rb
@@ -8,6 +8,8 @@ module PerfMonger
 module Command
 
 class RecordCommand < BaseCommand
+  LOCKFILE = File.expand_path(".perfmonger.lock", Dir.tmpdir())
+
   register_command 'record', 'Record system performance information'
 
   def initialize
@@ -17,13 +19,40 @@ class RecordCommand < BaseCommand
   def run(argv)
     @argv, @option = PerfMonger::Command::RecordOption.parse(argv)
 
+    session_file = File.expand_path(sprintf("perfmonger-%s-session.pid", Etc.getlogin),
+                                    Dir.tmpdir)
+    begin
+      session_pid = File.read(session_file).to_i
+    rescue Errno::ENOENT
+      # No session file
+      session_pid = nil
+    end
+
     if @option.kill
-      session_file = File.expand_path(sprintf("perfmonger-%s-session.pid", Etc.getlogin),
-                                      Dir.tmpdir)
-      Process.kill(:INT, File.read(session_file).to_i)
+      unless session_pid
+        # There is nothing to be killed
+        return true
+      end
+
+      begin
+        Process.kill(:INT, session_pid)
+      rescue Errno::ESRCH
+        # Session file has invalid (already dead) PID
+        File.open(LOCKFILE, "w") do |f|
+          f.flock(File::LOCK_EX)
+          FileUtils.rm(session_file)
+          f.flock(File::LOCK_UN)
+        end
+      end
     else
+      if session_pid && Process.getpgid(session_pid)
+        $stderr.puts("[ERROR] another perfmonger is already running.")
+        return false
+      end
       exec_record_cmd()
     end
+
+    true
   end
 
 private
