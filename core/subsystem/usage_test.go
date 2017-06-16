52a65edf2cceab8ed2d22ccad3516b9738f4a49b
Yuto Hayamizu <y.hayamizu@gmail.com>
2017-06-17 01:55:03 +0900

    disabled obsolete test specs

--- core/subsystem/usage_test.go
+++ core/subsystem/usage_test.go
@@ -65,6 +65,7 @@ func floatEqWithin(val1, val2, epsilon float64) bool {
 
 func TestGetCoreUsage(t *testing.T) {
 	var err error
+	var usage *CpuCoreUsage
 
 	c1 := new(CpuCoreStat)
 	c2 := new(CpuCoreStat)
@@ -73,10 +74,22 @@ func TestGetCoreUsage(t *testing.T) {
 	c2.User = 5
 	c2.Sys = 5
 
-	// should return error if uptime is the same
-	_, err = GetCpuCoreUsage(c1, c2)
-	if err == nil {
-		t.Error("err == nil, want non-nil")
+	// should return usage 0% with no error if uptime is the same
+	usage, err = GetCpuCoreUsage(c1, c2)
+	if err != nil {
+		t.Error("err != nil, want nil")
+	}
+	if !(usage.User == 0 &&
+		usage.Nice == 0 &&
+		usage.Sys == 0 &&
+		usage.Idle == 0 &&
+		usage.Iowait == 0 &&
+		usage.Hardirq == 0 &&
+		usage.Softirq == 0 &&
+		usage.Steal == 0 &&
+		usage.Guest == 0 &&
+		usage.GuestNice == 0) {
+		t.Error("usage is not 0%, want 0%")
 	}
 
 	// should return error if c1.Uptime() is larger than c2.Uptime()
@@ -87,7 +100,6 @@ func TestGetCoreUsage(t *testing.T) {
 	}
 
 	// should return 75% usr and 25% sys usage
-	var usage *CpuCoreUsage
 	c1.Clear()
 	c2.Clear()
 	c1.User = 100
@@ -124,11 +136,11 @@ func TestGetCpuUsage(t *testing.T) {
 	c2 := NewCpuStat(num_core)
 
 	usage, err = GetCpuUsage(c1, c2)
-	if err == nil {
-		t.Error("Error should be returned because no difference between c1 and c2")
+	if err != nil {
+		t.Error("Error should not be returned")
 	}
-	if usage != nil {
-		t.Error("Nil should be returned as usage")
+	if usage == nil {
+		t.Error("Nil should not be returned as usage")
 	}
 
 	c1.CoreStats[0].User = 100
@@ -141,13 +153,13 @@ func TestGetCpuUsage(t *testing.T) {
 	c2.CoreStats[1].User = c1.CoreStats[0].User + 300
 	c2.CoreStats[1].Sys = c1.CoreStats[0].Sys + 100
 
-	usage, err = GetCpuUsage(c1, c2)
-	if err == nil {
-		t.Error("Error should be returned because no progress in .All uptime")
-	}
-	if usage != nil {
-		t.Error("Nil should be returned as usage")
-	}
+	// usage, err = GetCpuUsage(c1, c2)
+	// if err == nil {
+	// 	t.Error("Error should be returned because no progress in .All uptime")
+	// }
+	// if usage != nil {
+	// 	t.Error("Nil should be returned as usage")
+	// }
 
 	c1.All.User = 200
 	c1.All.Sys = 100
@@ -158,12 +170,12 @@ func TestGetCpuUsage(t *testing.T) {
 	c2.CoreStats[0].Clear()
 
 	usage, err = GetCpuUsage(c1, c2)
-	if err == nil {
-		t.Error("Error should be returned because no progress in .CoreStats[9] uptime")
-	}
-	if usage != nil {
-		t.Error("Nil should be returned as usage")
-	}
+	// if err == nil {
+	// 	t.Error("Error should be returned because no progress in .CoreStats[9] uptime")
+	// }
+	// if usage != nil {
+	// 	t.Error("Nil should be returned as usage")
+	// }
 
 	c1.CoreStats[0].User = 100
 	c1.CoreStats[0].Sys = 50
