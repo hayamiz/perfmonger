ba21ef1604a804d6406651845f9110154eab0142
Yuto Hayamizu <y.hayamizu@gmail.com>
2017-06-24 00:29:28 +0900

    add test spec for --no-gzip option

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -12,4 +12,13 @@ describe '[record] subcommand' do
     expect("perfmonger.pgr.gz").to be_an_existing_file # default file name
     expect(last_command_started.stdout).to be_empty
   end
+
+  it 'should create a non-gzipped logfile' do
+    cmd = "#{perfmonger_bin} record --timeout 1 --no-gzip"
+    run(cmd)
+    expect(last_command_started).to be_successfully_executed
+    expect("perfmonger.pgr").to be_an_existing_file # default file name
+    expect(last_command_started.stdout).to be_empty
+  end
+
 end
